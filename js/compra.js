79faff1e3c45f924c9cfeb7f3e3fbe80b569ea06
LuiggiAndresZambranoFranco <74325889+LuiggiAndresZambranoFranco@users.noreply.github.com>
2021-03-14 14:59:48 -0500

    Add files via upload

---
+++ js/compra.js
@@ -0,0 +1,13 @@
+
+class fa fa-shopping-cart {
+
+    //Añadir producto al carrito
+    comprarProducto(e){
+        e.preventDefault();
+        //Delegado para agregar al carrito
+        if(e.target.classList.contains('fa fa-shopping-cart')){
+            const producto = e.target.parentElement.parentElement;
+            //Enviamos el producto seleccionado para tomar sus datos
+            this.leerDatosProducto(producto);
+        }
+    }
